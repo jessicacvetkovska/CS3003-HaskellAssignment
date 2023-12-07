b7e6c32970989513e62d136e283834c6624be8cd
jessicacvetkovska <jessicacvetkovska@gmail.com>
2023-12-07 13:40:10 -0500

    finished FindFirst

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -9,6 +9,20 @@ instance Show Found where
   show NoMatch = "No match found!"
 findFirst :: Eq a => (a -> Bool) -> [a] -> Found
 
+-- making a helper function which will keep track of the index with default value 0
+findFirst needle haystack = helper needle haystack 0 where
+  -- if list empty, return NoMatch
+  helper _ [] _ = NoMatch
+  helper needle (hay:stack) index =
+    -- if needle is true for the current element hay
+    -- return a Match with the current index
+    if needle hay
+      then Match index
+      -- otherwise, keep calling itself
+      -- with the remaining elements in the list
+      -- and increment index
+      else helper needle stack (index + 1)
+
 ------------------------------------------------
 -- palindrome
 ------------------------------------------------
