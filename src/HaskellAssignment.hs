4dee4f8363aed91d05a74b65d48f8255dee4dc38
jessicacvetkovska <jessicacvetkovska@gmail.com>
2023-12-07 13:18:52 -0500

    initial commit + palindrome completion

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -13,3 +13,12 @@ findFirst :: Eq a => (a -> Bool) -> [a] -> Found
 -- palindrome
 ------------------------------------------------
 palindrome :: [Char] -> Bool
+
+-- an empty string is a palindrome!
+palindrome [] = True
+
+-- reverse candidate will reverse the list
+-- candidate == reverse candidate will check if the original
+-- list is equal to the reversed list, which indicates that
+-- it is a palindrome
+palindrome candidate = candidate == reverse candidate
